76663e5b15fef4dbadcebabecc06d66647922b8c
JakeMassmann <JakeMassmann@gmail.com>
2019-01-31 20:37:40 -0500

    css updates, search bar mockup

--- client/src/components/BrowseShortStories.js
+++ client/src/components/BrowseShortStories.js
@@ -1,23 +1,65 @@
 import React, { Component } from 'react';
 import ShortStoryList from './ShortStoryList';
 import {
+	Button,
 	Container,
 	Card,
 	CardHeader,
 	CardBody,
-	CardTitle
+	CardTitle,
+	Input,
+	InputGroup,
+	InputGroupButtonDropdown,
+	InputGroupAddon,
+	DropdownMenu,
+	DropdownToggle,
+	DropdownItem
 } from 'reactstrap';
 
 class BrowseShortStories extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggleDropDown = this.toggleDropDown.bind(this);
+    this.toggleSplit = this.toggleSplit.bind(this);
+    this.state = {
+      dropdownOpen: false,
+      splitButtonOpen: false
+    };
+  }
+
+  toggleDropDown() {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen
+    });
+  }
+
+  toggleSplit() {
+    this.setState({
+      splitButtonOpen: !this.state.splitButtonOpen
+    });
+  }
+
   render() {
     return (
       <Container style={{height: '100%'}}>
-      		<Card  className="m-3 shadow-2">
-      			<CardHeader tag="h1">
-      				Find Stories
-      			</CardHeader>
-      		</Card>
-	        <ShortStoryList user={null} readOnly={true} />
+
+        <InputGroup size="lg">
+          <InputGroupButtonDropdown addonType="prepend" isOpen={this.state.dropdownOpen} toggle={this.toggleDropDown}>
+            <DropdownToggle caret>
+              Genre
+            </DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem>Horror</DropdownItem>
+              <DropdownItem>Romance</DropdownItem>
+            </DropdownMenu>
+          </InputGroupButtonDropdown>
+          <Input />
+           <InputGroupAddon addonType="append"><Button color="primary">{"  Search   "}</Button></InputGroupAddon>
+        </InputGroup>
+
+        <ShortStoryList user={null} readOnly={true} />
+
 	  </Container> 
     );
   }
